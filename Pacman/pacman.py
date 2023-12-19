e9e574abe44beee45a60a821db6ce179992183e9
JeffreyLee <s10258143@connect.np.edu.sg>
2023-12-19 16:59:14 +0800

    Multiplayer Mode: Added Player 2 Sprite, Modified Game Logic to account for 2nd player

--- Pacman/pacman.py
+++ Pacman/pacman.py
@@ -369,7 +369,7 @@ b_h = (3*60)+19 #Binky height
 i_w = 303-16-32 #Inky width
 c_w = 303+(32-16) #Clyde width
 
-def startGame():
+def startSingleplayerGame():
 
   username = nameEntry(screen)
   all_sprites_list = pygame.sprite.RenderPlain()
@@ -531,14 +531,219 @@ def startGame():
       if score == bll:
         with open ('scores.csv', 'a') as file:
             file.write(username + " , " + str(score) +'\n')
-        doNext("Congratulations, you won!",145,all_sprites_list,block_list,monsta_list,pacman_collide,wall_list,gate)
+        doNextSingleplayer("Congratulations, you won!",145,all_sprites_list,block_list,monsta_list,pacman_collide,wall_list,gate)
 
       monsta_hit_list = pygame.sprite.spritecollide(Pacman, monsta_list, False)
 
       if monsta_hit_list:
         with open ('scores.csv', 'a') as file:
             file.write(username + " , " + str(score) +'\n')
-        doNext("Game Over",235,all_sprites_list,block_list,monsta_list,pacman_collide,wall_list,gate)
+        doNextSingleplayer("Game Over",235,all_sprites_list,block_list,monsta_list,pacman_collide,wall_list,gate)
+
+      # ALL CODE TO DRAW SHOULD GO ABOVE THIS COMMENT
+      
+      pygame.display.flip()
+    
+      clock.tick(10)
+
+def startMultiplayerGame():
+
+  username = nameEntry(screen)
+  all_sprites_list = pygame.sprite.RenderPlain()
+
+  block_list = pygame.sprite.RenderPlain()
+
+  monsta_list = pygame.sprite.RenderPlain()
+
+  pacman_collide = pygame.sprite.RenderPlain()
+
+  pacman2_collide = pygame.sprite.RenderPlain()
+
+  wall_list = setupRoomOne(all_sprites_list)
+
+  gate = setupGate(all_sprites_list)
+
+
+  p_turn = 0
+  p_steps = 0
+
+  b_turn = 0
+  b_steps = 0
+
+  i_turn = 0
+  i_steps = 0
+
+  c_turn = 0
+  c_steps = 0
+
+
+  # Create the player paddle object
+  Pacman = Player( w, p_h, "Pacman/images/Overflow.png" )
+  all_sprites_list.add(Pacman)
+  pacman_collide.add(Pacman)
+   
+  Pacman2 = Player( w, p_h, "Pacman/images/Overflow2.png" )
+  all_sprites_list.add(Pacman2)
+  pacman2_collide.add(Pacman2)
+   
+  Blinky=Ghost( w, b_h, "Pacman/images/AmongUsGreen.png" )
+  monsta_list.add(Blinky)
+  all_sprites_list.add(Blinky)
+
+  Pinky=Ghost( w, m_h, "Pacman/images/AmongUsRed.png" )
+  monsta_list.add(Pinky)
+  all_sprites_list.add(Pinky)
+   
+  Inky=Ghost( i_w, m_h, "Pacman/images/AmongUsYellow.png" )
+  monsta_list.add(Inky)
+  all_sprites_list.add(Inky)
+   
+  Clyde=Ghost( c_w, m_h, "Pacman/images/AmongUsOrange.png" )
+  monsta_list.add(Clyde)
+  all_sprites_list.add(Clyde)
+
+  # Draw the grid
+  for row in range(19):
+      for column in range(19):
+          if (row == 7 or row == 8) and (column == 8 or column == 9 or column == 10):
+              continue
+          else:
+            block = Block(yellow, 4, 4)
+
+            # Set a random location for the block
+            block.rect.x = (30*column+6)+26
+            block.rect.y = (30*row+6)+26
+
+            b_collide = pygame.sprite.spritecollide(block, wall_list, False)
+            p_collide = pygame.sprite.spritecollide(block, pacman_collide, False)
+            p2_collide = pygame.sprite.spritecollide(block, pacman2_collide, False)
+            if b_collide:
+              continue
+            elif p_collide:
+              continue
+            else:
+              # Add the block to the list of objects
+              block_list.add(block)
+              all_sprites_list.add(block)
+
+  bll = len(block_list)
+
+  score = 0
+
+  done = False
+
+  i = 0
+
+  while done == False:
+      # ALL EVENT PROCESSING SHOULD GO BELOW THIS COMMENT
+      for event in pygame.event.get():
+          if event.type == pygame.QUIT:
+              done=True
+
+          if event.type == pygame.KEYDOWN:
+              if event.key == pygame.K_LEFT:
+                  Pacman.changespeed(-30,0)
+              if event.key == pygame.K_RIGHT:
+                  Pacman.changespeed(30,0)
+              if event.key == pygame.K_UP:
+                  Pacman.changespeed(0,-30)
+              if event.key == pygame.K_DOWN:
+                  Pacman.changespeed(0,30)
+              if event.key == 97:
+                  print(event.key)
+                  Pacman2.changespeed(-30,0)
+              if event.key == 100:
+                  print(event.key)
+                  Pacman2.changespeed(30,0)
+              if event.key == 119:
+                  Pacman2.changespeed(0,-30)
+              if event.key == 115:
+                  Pacman2.changespeed(0,30)
+
+          if event.type == pygame.KEYUP:
+              if event.key == pygame.K_LEFT:
+                  Pacman.changespeed(30,0)
+              if event.key == pygame.K_RIGHT:
+                  Pacman.changespeed(-30,0)
+              if event.key == pygame.K_UP:
+                  Pacman.changespeed(0,30)
+              if event.key == pygame.K_DOWN:
+                  Pacman.changespeed(0,-30)
+              if event.key == 97:
+                  Pacman2.changespeed(30,0)
+              if event.key == 100:
+                  Pacman2.changespeed(-30,0)
+              if event.key == 119:
+                  Pacman2.changespeed(0,30)
+              if event.key == 115:
+                  Pacman2.changespeed(0,-30)
+          
+      # ALL EVENT PROCESSING SHOULD GO ABOVE THIS COMMENT
+   
+      # ALL GAME LOGIC SHOULD GO BELOW THIS COMMENT
+      Pacman.update(wall_list,gate)
+      Pacman2.update(wall_list,gate)
+
+      returned = Pinky.changespeed(Pinky_directions,False,p_turn,p_steps,pl)
+      p_turn = returned[0]
+      p_steps = returned[1]
+      Pinky.changespeed(Pinky_directions,False,p_turn,p_steps,pl)
+      Pinky.update(wall_list,False)
+
+      returned = Blinky.changespeed(Blinky_directions,False,b_turn,b_steps,bl)
+      b_turn = returned[0]
+      b_steps = returned[1]
+      Blinky.changespeed(Blinky_directions,False,b_turn,b_steps,bl)
+      Blinky.update(wall_list,False)
+
+      returned = Inky.changespeed(Inky_directions,False,i_turn,i_steps,il)
+      i_turn = returned[0]
+      i_steps = returned[1]
+      Inky.changespeed(Inky_directions,False,i_turn,i_steps,il)
+      Inky.update(wall_list,False)
+
+      returned = Clyde.changespeed(Clyde_directions,"clyde",c_turn,c_steps,cl)
+      c_turn = returned[0]
+      c_steps = returned[1]
+      Clyde.changespeed(Clyde_directions,"clyde",c_turn,c_steps,cl)
+      Clyde.update(wall_list,False)
+
+      # See if the Pacman block has collided with anything.
+      blocks_hit_list = pygame.sprite.spritecollide(Pacman, block_list, True)
+
+      # See if the Pacman2 block has collided with anything.
+      blocks_hit_list = pygame.sprite.spritecollide(Pacman2, block_list, True)
+       
+      # Check the list of collisions.
+      if len(blocks_hit_list) > 0:
+          score +=len(blocks_hit_list)
+      
+          
+      # ALL GAME LOGIC SHOULD GO ABOVE THIS COMMENT
+   
+      # ALL CODE TO DRAW SHOULD GO BELOW THIS COMMENT
+      screen.fill(black)
+        
+      wall_list.draw(screen)
+      gate.draw(screen)
+      all_sprites_list.draw(screen)
+      monsta_list.draw(screen)
+    
+      
+
+      text=font.render("Score: "+str(score)+"/"+str(bll), True, red)
+      screen.blit(text, [10, 10])
+
+      if score == bll:
+        with open ('scores.csv', 'a') as file:
+            file.write(username + " , " + str(score) +'\n')
+        doNextMultiplayer("Congratulations, you won!",145,all_sprites_list,block_list,monsta_list,pacman_collide,wall_list,gate)
+
+      monsta_hit_list = pygame.sprite.spritecollide(Pacman, monsta_list, False)
+      monsta_hit_list2 = pygame.sprite.spritecollide(Pacman2, monsta_list, False)
+
+      if monsta_hit_list or monsta_hit_list2:
+        doNextMultiplayer("Game Over",235,all_sprites_list,block_list,monsta_list,pacman_collide,wall_list,gate)
 
       # ALL CODE TO DRAW SHOULD GO ABOVE THIS COMMENT
       
@@ -574,7 +779,7 @@ def nameEntry(screen):
   return name
 
 
-def doNext(message,left,all_sprites_list,block_list,monsta_list,pacman_collide,wall_list,gate):
+def doNextSingleplayer(message,left,all_sprites_list,block_list,monsta_list,pacman_collide,wall_list,gate):
   while True:
       # ALL EVENT PROCESSING SHOULD GO BELOW THIS COMMENT
       for event in pygame.event.get():
@@ -591,7 +796,46 @@ def doNext(message,left,all_sprites_list,block_list,monsta_list,pacman_collide,w
             del pacman_collide
             del wall_list
             del gate
-            startGame()
+            startSingleplayerGame()
+
+      #Grey background
+      w = pygame.Surface((400,200))  # the size of your rect
+      w.set_alpha(10)                # alpha level
+      w.fill((128,128,128))           # this fills the entire surface
+      screen.blit(w, (100,200))    # (0,0) are the top-left coordinates
+
+      #Won or lost
+      text1=font.render(message, True, white)
+      screen.blit(text1, [left, 233])
+
+      text2=font.render("To play again, press ENTER.", True, white)
+      screen.blit(text2, [135, 303])
+      text3=font.render("To quit, press ESCAPE.", True, white)
+      screen.blit(text3, [165, 333])
+
+      pygame.display.flip()
+
+      clock.tick(10)
+
+
+def doNextMultiplayer(message,left,all_sprites_list,block_list,monsta_list,pacman_collide,wall_list,gate):
+  while True:
+      # ALL EVENT PROCESSING SHOULD GO BELOW THIS COMMENT
+      for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+          pygame.quit()
+        if event.type == pygame.KEYDOWN:
+          if event.key == pygame.K_ESCAPE:
+            mainMenu(screen)
+            
+          if event.key == pygame.K_RETURN:
+            del all_sprites_list
+            del block_list
+            del monsta_list
+            del pacman_collide
+            del wall_list
+            del gate
+            startMultiplayerGame()
 
       #Grey background
       w = pygame.Surface((400,200))  # the size of your rect
@@ -727,7 +971,7 @@ def scoreBoard():
 def mainMenu(screen):
     menu_font = pygame.font.Font(None, 36)
     selected_option = 0
-    menu_options = ["Start Game","Scoreboard", "Quit"]
+    menu_options = ["Start Singleplayer Game", "Start Multiplayer Game", "Scoreboard", "Quit"]
     username = ""
 
     while True:
@@ -741,10 +985,12 @@ def mainMenu(screen):
                     selected_option = (selected_option + 1) % len(menu_options)
                 elif event.key == pygame.K_RETURN:
                     if selected_option == 0:  # Start Game selected
-                        startGame()
+                        startSingleplayerGame()
                     elif selected_option == 1:  # Quit selected
+                        startMultiplayerGame()
+                    elif selected_option == 2:  # Quit selected
                         scoreBoard()
-                    elif selected_option == 2:
+                    elif selected_option == 3:
                         quit()
                     
                         
@@ -772,7 +1018,7 @@ def main():
     username = mainMenu(screen)
     if username:
         print("Username entered:", username)
-        startGame()
+        startSingleplayerGame()
 
     pygame.quit()
 
